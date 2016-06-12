eaad6d2aefb89c482e97b14fe773460144bf0b70
radical_edo <radical.3do@gmail.com>
2016-06-12 10:45:11 +0200

    action mixing

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+import { chain, throttle } from 'lodash';
+
+const getActionName = function getActionName(name) {
+  return chain(name).camelCase().upperFirst().value();
+};
+
+const ActionMixin = {
+  onThrottledAction(name, options = { wait: 0 }, ...params) {
+    return throttle((...args) => {
+      this['onThrottledAction' + getActionName(name)].apply(this, args.concat(params));
+    }, options.wait, options);
+  },
+
+  onAction(name, ...params) {
+    return (...args) => {
+      this['onAction' + getActionName(name)].apply(this, args.concat(params));
+    };
+  }
+};
+
+export default ActionMixin;
